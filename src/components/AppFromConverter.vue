265cda8dad93ca13f6ccc161d9bb31d880153670
alex02r <alex.giochi02@gmail.com>
2024-05-06 19:20:01 +0200

    uograde: struttura html form converter

--- src/components/AppFromConverter.vue
+++ src/components/AppFromConverter.vue
@@ -1,11 +1,35 @@
 <script>
 export default {
-    
+    props:{
+        currencies: Object
+    }
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Currency Converter</h1>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="">
+                    <h4>Resoconto</h4>
+                    <h2>Valuta</h2>
+                </div>
+                <div class="">
+                    <input type="text" name="" id="" class="">
+                    <select name="" id="">
+                        <option value="">Prova</option>
+                    </select>
+                </div>
+                <div class="">
+                    <input type="text" name="" id="" class="">
+                    <select name="" id="">
+                        <option value="">Prova</option>
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="">
