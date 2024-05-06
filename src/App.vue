709c2fcd8217a22933bb6eb27ccb09c422524404
alex02r <alex.giochi02@gmail.com>
2024-05-06 18:57:06 +0200

    upgrade: import component AppFormConverter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script>
+import AppFromConverter from './components/AppFromConverter.vue';
 export default {
-  
+  components:{
+    AppFromConverter
+  }
 }
 </script>
 <template lang="">
-  <div>
-    
-  </div>
+  <main>
+    <AppFromConverter/>
+  </main>
 </template>
 <style lang="scss">
   @use './styles/generals.scss';
