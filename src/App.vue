a0a2cfb9ab072404e750a7ee85e028ac51849c89
alex02r <alex.giochi02@gmail.com>
2024-05-06 19:19:42 +0200

    upgrade: funzione che restituisce la lista di tutte le valute

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <script>
+import axios from 'axios';
+import { store } from './store';
 import AppFromConverter from './components/AppFromConverter.vue';
 export default {
   components:{
     AppFromConverter
-  }
+  },
+  data() {
+    return {
+      store,
+      currencies: {},
+    }
+  },
+  mounted() {
+    this.getCurrencies();
+  },
+  methods: {
+    //funzione che prende l'oggetto di tutte le valute
+    getCurrencies(){
+      axios.get(store.api_frankfurter+'currencies').then( response =>{
+        this.currencies = response.data;
+      })
+    }
+  },
 }
 </script>
 <template lang="">
   <main>
-    <AppFromConverter/>
+    <AppFromConverter :currencies="currencies" />
   </main>
 </template>
 <style lang="scss">
